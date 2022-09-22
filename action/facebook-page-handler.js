96fa431e3e35ab921c8a35c353dd5af3f65d632e
Sirichai Janpan <13_oy@hotmail.co.th>
2022-09-22 22:02:04 +0700

    fixed bug

--- action/facebook-page-handler.js
+++ action/facebook-page-handler.js
@@ -4,7 +4,6 @@ const {getCalPlusFarm, getKathy} = require("../src/services/facebook-pages/faceb
 module.exports.callFacebookPage = async () => {
     return async () => {
         await getKathy();
-        await new Promise(resolve => setTimeout(resolve, 3000)); // wait 3s
         await  getCalPlusFarm();
     };
 }
