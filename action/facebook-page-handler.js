481fe0ecf8c137a0338b8591b5d997b8d3f66da2
Sirichai Janpan <13_oy@hotmail.co.th>
2022-09-22 22:06:07 +0700

    fixed

--- action/facebook-page-handler.js
+++ action/facebook-page-handler.js
@@ -2,8 +2,8 @@
 const {getCalPlusFarm, getKathy} = require("../src/services/facebook-pages/facebook");
 
 module.exports.callFacebookPage = async () => {
-    return async () => {
-        await getKathy();
-        await  getCalPlusFarm();
-    };
+    await getKathy();
+    await new Promise(resolve => setTimeout(resolve, 3000)); // wait 3s
+    await getCalPlusFarm();
+    return true;
 }
